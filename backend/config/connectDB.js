38a94e23831a3caa009f6c36a384f9f31d686487
MANYA SHUKLA <157141586+MANYA-SHUKLA@users.noreply.github.com>
2026-01-30 00:32:15 +0530

    Fix formatting in connectDB.js

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
 const path = require('path');
-
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 if(!process.env.MONGO_URI){
